fad88472a1c3d7fc7b4fd7a816844a24126d640b
Priyanshu-PM <priyanshumahukhaye@gmail.com>
2023-10-10 14:16:16 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <p className='text-3xl font-bold text-red-500'>Hello world</p>
+    </div>
+  )
+}
+
+export default App
