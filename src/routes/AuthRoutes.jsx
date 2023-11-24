4a32b9288c7b4f079cddfb19c7edb9c631d26a9d
AbhijitKhyade <abhijitkhyade@gmail.com>
2023-11-24 18:51:30 +0530

    Authentication part

--- src/routes/AuthRoutes.jsx
+++ src/routes/AuthRoutes.jsx
@@ -1,14 +1,19 @@
-import Login from '../pages/Auth/login';
-import Auth from './../Layouts/Auth';
+import Login from "../pages/Auth/Login";
+import Register from "../pages/Auth/Register";
+import Auth from "./../Layouts/Auth";
 
-export const AuthRoutes =  {
-    path: "auth/",
-    element:<Auth />,
-    // errorElement: <ErrorPage/>,
-    children:[
-        {
-            path:'login',
-            element:<Login/>
-        },
-    ]
-}
+export const AuthRoutes = {
+  path: "auth/",
+  element: <Auth />,
+  // errorElement: <ErrorPage/>,
+  children: [
+    {
+      path: "login",
+      element: <Login />,
+    },
+    {
+      path: "register",
+      element: <Register />,
+    },
+  ],
+};
